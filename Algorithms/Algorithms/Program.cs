f1df6f7166d42353c5b286cd108574210b1acf5d
Tech Lead <techlead@iara.tech>
2022-03-06 18:02:13 -0300

    Algoritmos exercises chapter 4

---
+++ Algorithms/Algorithms/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Algorithms;
+
+int[] array = new int[5] { 5, 3, 6, 2, 10 };
+
+Console.WriteLine(String.Join(",", new SelectionSort().Sort(array)));
---
+++ Algorithms/Algorithms/SelectionSort.cs
@@ -0,0 +1,52 @@
+﻿namespace Algorithms
+{
+    internal class SelectionSort
+    {
+        public int[] Sort(int[] array)
+        {
+            int[] newArray = new int[array.Length];
+            int indexMinor;
+            int indexAppend = 0;
+            int length = array.Length;
+            for (int i = 0; i < length; i++)
+            {
+                indexMinor = SearchMinor(array);
+                newArray[indexAppend] = array[indexMinor];
+                indexAppend++;
+                array = RemoveItemByIndex(array, indexMinor);
+            }
+
+            return newArray;
+        }
+
+        private int[] RemoveItemByIndex(int[] array, int indexMinor)
+        {
+            int[] newArray = new int[array.Length - 1];
+            int index = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i != indexMinor)
+                {
+                    newArray[index] = array[i];
+                    index++;
+                }
+            }
+            return newArray;
+        }
+
+        private int SearchMinor(int[] array)
+        {
+            int minor = array[0];
+            int index = 0;
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minor)
+                {
+                    minor = array[i];
+                    index = i;
+                }
+            }
+            return index;
+        }
+    }
+}
